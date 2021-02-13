099595e550cb085557d94097d3826bc1198441b7
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2021-02-13 13:29:39 +0200

    Add project files.

---
+++ Tracking.Finance.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tracking.Finance.Web.Data
+{
+	public class ApplicationDbContext : IdentityDbContext
+	{
+		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+			: base(options)
+		{
+		}
+	}
+}
