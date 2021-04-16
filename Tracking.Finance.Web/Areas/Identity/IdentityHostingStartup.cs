c99294e48ca70da8e0f9c27864c4c60ac176ea3a
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2021-04-16 23:21:18 +0300

    Fix identity scaffolding formatting, add initial user

--- Tracking.Finance.Web/Areas/Identity/IdentityHostingStartup.cs
+++ Tracking.Finance.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -1,21 +1,17 @@
-﻿using System;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Tracking.Finance.Web.Data;
+﻿using Microsoft.AspNetCore.Hosting;
 
 [assembly: HostingStartup(typeof(Tracking.Finance.Web.Areas.Identity.IdentityHostingStartup))]
+
 namespace Tracking.Finance.Web.Areas.Identity
 {
-    public class IdentityHostingStartup : IHostingStartup
-    {
-        public void Configure(IWebHostBuilder builder)
-        {
-            builder.ConfigureServices((context, services) => {
-            });
-        }
-    }
+	public class IdentityHostingStartup : IHostingStartup
+	{
+		/// <inheritdoc/>
+		public void Configure(IWebHostBuilder builder)
+		{
+			builder.ConfigureServices((context, services) =>
+			{
+			});
+		}
+	}
 }
