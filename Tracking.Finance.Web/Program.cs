afaed2af8450e4da64279b2883fbff6bf9f25887
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2021-05-19 21:29:43 +0300

    Remove DB seeding

--- Tracking.Finance.Web/Program.cs
+++ Tracking.Finance.Web/Program.cs
@@ -1,15 +1,7 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 using Tracking.Finance.Web;
-using Tracking.Finance.Web.Data;
 
-var host = WebHost.CreateDefaultBuilder<Startup>(args).Build();
-//using (var scope = host.Services.CreateScope())
-//{
-//	await scope.ServiceProvider.SeedDatabase();
-//}
-
-host.Run();
+WebHost.CreateDefaultBuilder<Startup>(args).Build().Run();
