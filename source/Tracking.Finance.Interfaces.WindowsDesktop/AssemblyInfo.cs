d636871455c4748e6c8462a558dfbb70c35b4457
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2021-07-11 17:42:51 +0300

    Delete WPF desktop project

--- source/Tracking.Finance.Interfaces.WindowsDesktop/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// Copyright 2021 Valters Melnalksnis
-// Licensed under the GNU Affero General Public License v3.0 or later.
-// See LICENSE.txt file in the project root for full license information.
-
-using System.Windows;
-
-[assembly: ThemeInfo(
-	ResourceDictionaryLocation.None,
-	ResourceDictionaryLocation.SourceAssembly)]
