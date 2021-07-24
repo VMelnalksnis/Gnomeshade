49e439735724ed8686078947c214f37a8eb24a9b
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2021-07-24 14:29:05 +0300

    Add FidavistaReader

---
+++ source/Tracking.Finance/Imports/Fidavista/CurrencyStatement.cs
@@ -0,0 +1,24 @@
+﻿// Copyright 2021 Valters Melnalksnis
+// Licensed under the GNU Affero General Public License v3.0 or later.
+// See LICENSE.txt file in the project root for full license information.
+
+using System.Xml.Serialization;
+
+namespace Tracking.Finance.Imports.Fidavista
+{
+	[XmlRoot("CcyStmt")]
+	public sealed record CurrencyStatement
+	{
+		[XmlElement("Ccy")]
+		public string Currency { get; init; } = null!;
+
+		[XmlElement("OpenBal")]
+		public decimal OpeningBalance { get; init; }
+
+		[XmlElement("CloseBal")]
+		public decimal? ClosingBalance { get; init; }
+
+		[XmlElement("TrxSet")]
+		public Transaction[]? Transactions { get; init; }
+	}
+}
