8eb939674fde193664baefd853ad76b996089874
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2021-07-26 21:15:54 +0300

    Fix namespace in test resources

--- tests/Gnomeshade.Core.Tests/Imports/Fidavista/TestFiles.Designer.cs
+++ tests/Gnomeshade.Core.Tests/Imports/Fidavista/TestFiles.Designer.cs
@@ -39,7 +39,7 @@ namespace Gnomeshade.Core.Tests.Imports.Fidavista {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("VMelnalksnis.Gnomeshade.Core.Tests.Imports.Fidavista.TestFiles", typeof(TestFiles).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Gnomeshade.Core.Tests.Imports.Fidavista.TestFiles", typeof(TestFiles).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
