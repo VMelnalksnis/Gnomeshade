60f2a10ec5912334caf4de09c47fdd599fd34ee2
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-03-17 23:07:49 +0200

    Add tags when creating transaction item

--- source/Gnomeshade.Interfaces.Avalonia.Core/Transactions/TransactionItemCreationViewModel.cs
+++ source/Gnomeshade.Interfaces.Avalonia.Core/Transactions/TransactionItemCreationViewModel.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,6 +12,7 @@ using Avalonia.Collections;
 using Avalonia.Controls;
 
 using Gnomeshade.Interfaces.Avalonia.Core.Products;
+using Gnomeshade.Interfaces.Avalonia.Core.Tags;
 using Gnomeshade.Interfaces.WebApi.Client;
 using Gnomeshade.Interfaces.WebApi.Models.Accounts;
 using Gnomeshade.Interfaces.WebApi.Models.Products;
@@ -19,9 +21,7 @@ using Gnomeshade.Interfaces.WebApi.Models.Transactions;
 
 namespace Gnomeshade.Interfaces.Avalonia.Core.Transactions;
 
-/// <summary>
-/// Form for creating a single new transaction item.
-/// </summary>
+/// <summary>Form for creating a single new transaction item.</summary>
 public class TransactionItemCreationViewModel : ViewModelBase
 {
 	private readonly IGnomeshadeClient _transactionClient;
@@ -40,7 +40,8 @@ public class TransactionItemCreationViewModel : ViewModelBase
 	private string? _internalReference;
 	private ProductCreationViewModel? _productCreation;
 	private List<Product> _products;
-	private Tag _tag;
+	private Tag? _tag;
+	private DataGridItemCollectionView<TagRow> _tagRows;
 
 	private TransactionItemCreationViewModel(
 		IGnomeshadeClient transactionClient,
@@ -55,10 +56,14 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		Tags = tags;
 		_products = products;
 
+		_tagRows = new(Array.Empty<TagRow>());
+
 		AccountSelector = (_, item) => ((Account)item).Name;
 		CurrencySelector = (_, item) => ((Currency)item).AlphabeticCode;
 		ProductSelector = (_, item) => ((Product)item).Name;
 		TagSelector = (_, item) => ((Tag)item).Name;
+
+		PropertyChanged += OnPropertyChanged;
 	}
 
 	private TransactionItemCreationViewModel(
@@ -67,7 +72,8 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		List<Currency> currencies,
 		List<Product> products,
 		List<Tag> tags,
-		TransactionItem item)
+		TransactionItem item,
+		List<Tag> existingTags)
 		: this(transactionClient, accounts, currencies, products, tags)
 	{
 		_existingItem = item;
@@ -83,50 +89,35 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		BankReference = item.BankReference;
 		ExternalReference = item.ExternalReference;
 		InternalReference = item.InternalReference;
+		TagRows = new(existingTags.Select(tag => new TagRow(tag.Id, tag.Name, tag.Description)));
 	}
 
-	/// <summary>
-	/// Raised when a new transaction item has been successfully created.
-	/// </summary>
+	/// <summary>Raised when a new transaction item has been successfully created.</summary>
 	public event EventHandler<TransactionItemCreatedEventArgs>? TransactionItemCreated;
 
-	/// <summary>
-	/// Gets a collection of all active accounts.
-	/// </summary>
+	/// <summary>Gets a collection of all active accounts.</summary>
 	public List<Account> Accounts { get; }
 
-	/// <summary>
-	/// Gets a delegate for formatting an account in an <see cref="AutoCompleteBox"/>.
-	/// </summary>
+	/// <summary>Gets a delegate for formatting an account in an <see cref="AutoCompleteBox"/>.</summary>
 	public AutoCompleteSelector<object> AccountSelector { get; }
 
-	/// <summary>
-	/// Gets a collection of all currencies.
-	/// </summary>
+	/// <summary>Gets a collection of all currencies.</summary>
 	public List<Currency> Currencies { get; }
 
-	/// <summary>
-	/// Gets a delegate for formatting a currency in an <see cref="AutoCompleteBox"/>.
-	/// </summary>
+	/// <summary>Gets a delegate for formatting a currency in an <see cref="AutoCompleteBox"/>.</summary>
 	public AutoCompleteSelector<object> CurrencySelector { get; }
 
-	/// <summary>
-	/// Gets a collection of all products.
-	/// </summary>
+	/// <summary>Gets a collection of all products.</summary>
 	public List<Product> Products
 	{
 		get => _products;
 		private set => SetAndNotify(ref _products, value, nameof(Products));
 	}
 
-	/// <summary>
-	/// Gets a delegate for formatting a product in an <see cref="AutoCompleteBox"/>.
-	/// </summary>
+	/// <summary>Gets a delegate for formatting a product in an <see cref="AutoCompleteBox"/>.</summary>
 	public AutoCompleteSelector<object> ProductSelector { get; }
 
-	/// <summary>
-	/// Gets or sets the source account of the transaction item.
-	/// </summary>
+	/// <summary>Gets or sets the source account of the transaction item.</summary>
 	public Account? SourceAccount
 	{
 		get => _sourceAccount;
@@ -140,9 +131,7 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		}
 	}
 
-	/// <summary>
-	/// Gets or sets the amount withdrawn from <see cref="SourceAccount"/>.
-	/// </summary>
+	/// <summary>Gets or sets the amount withdrawn from <see cref="SourceAccount"/>.</summary>
 	public decimal? SourceAmount
 	{
 		get => _sourceAmount;
@@ -156,9 +145,7 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		}
 	}
 
-	/// <summary>
-	/// Gets or sets the currency of <see cref="SourceAmount"/>.
-	/// </summary>
+	/// <summary>Gets or sets the currency of <see cref="SourceAmount"/>.</summary>
 	public Currency? SourceCurrency
 	{
 		get => _sourceCurrency;
@@ -170,9 +157,7 @@ public class TransactionItemCreationViewModel : ViewModelBase
 			nameof(IsTargetAmountReadOnly));
 	}
 
-	/// <summary>
-	/// Gets or sets the target account of the transaction item.
-	/// </summary>
+	/// <summary>Gets or sets the target account of the transaction item.</summary>
 	public Account? TargetAccount
 	{
 		get => _targetAccount;
@@ -192,23 +177,17 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		}
 	}
 
-	/// <summary>
-	/// Gets or sets the amount deposited to <see cref="TargetAccount"/>.
-	/// </summary>
+	/// <summary>Gets or sets the amount deposited to <see cref="TargetAccount"/>.</summary>
 	public decimal? TargetAmount
 	{
 		get => _targetAmount;
 		set => SetAndNotifyWithGuard(ref _targetAmount, value, nameof(TargetAmount), nameof(CanCreate));
 	}
 
-	/// <summary>
-	/// Gets a value indicating whether <see cref="TargetAmount"/> should not be editable.
-	/// </summary>
+	/// <summary>Gets a value indicating whether <see cref="TargetAmount"/> should not be editable.</summary>
 	public bool IsTargetAmountReadOnly => SourceCurrency == TargetCurrency;
 
-	/// <summary>
-	/// Gets or sets the currency of <see cref="TargetAmount"/>.
-	/// </summary>
+	/// <summary>Gets or sets the currency of <see cref="TargetAmount"/>.</summary>
 	public Currency? TargetCurrency
 	{
 		get => _targetCurrency;
@@ -220,75 +199,71 @@ public class TransactionItemCreationViewModel : ViewModelBase
 			nameof(IsTargetAmountReadOnly));
 	}
 
-	/// <summary>
-	/// Gets or sets the product of the transaction item.
-	/// </summary>
+	/// <summary>Gets or sets the product of the transaction item.</summary>
 	public Product? Product
 	{
 		get => _product;
 		set => SetAndNotifyWithGuard(ref _product, value, nameof(Product), nameof(CanCreate));
 	}
 
-	/// <summary>
-	/// Gets or sets the amount of <see cref="Product"/>.
-	/// </summary>
+	/// <summary>Gets or sets the amount of <see cref="Product"/>.</summary>
 	public decimal? Amount
 	{
 		get => _amount;
 		set => SetAndNotifyWithGuard(ref _amount, value, nameof(Amount), nameof(CanCreate));
 	}
 
-	/// <summary>
-	/// Gets or sets the bank reference of the transaction item.
-	/// </summary>
+	/// <summary>Gets or sets the bank reference of the transaction item.</summary>
 	public string? BankReference
 	{
 		get => _bankReference;
 		set => SetAndNotify(ref _bankReference, value, nameof(BankReference));
 	}
 
-	/// <summary>
-	/// Gets or sets the external reference of the transaction item.
-	/// </summary>
+	/// <summary>Gets or sets the external reference of the transaction item.</summary>
 	public string? ExternalReference
 	{
 		get => _externalReference;
 		set => SetAndNotify(ref _externalReference, value, nameof(ExternalReference));
 	}
 
-	/// <summary>
-	/// Gets or sets the internal reference of the transaction item.
-	/// </summary>
+	/// <summary>Gets or sets the internal reference of the transaction item.</summary>
 	public string? InternalReference
 	{
 		get => _internalReference;
 		set => SetAndNotify(ref _internalReference, value, nameof(InternalReference));
 	}
 
-	public List<Tag> Tags { get; set; }
+	/// <summary>Gets a list of all available tags.</summary>
+	public List<Tag> Tags { get; }
 
-	public DataGridCollectionView TagsToAdd { get; set; }
+	public DataGridCollectionView TagsToAdd => TagRows;
 
+	public DataGridItemCollectionView<TagRow> TagRows
+	{
+		get => _tagRows;
+		private set => SetAndNotifyWithGuard(ref _tagRows, value, nameof(TagRows), nameof(TagsToAdd));
+	}
+
+	/// <summary>Gets a delegate for formatting a tag in an <see cref="AutoCompleteBox"/>.</summary>
 	public AutoCompleteSelector<object> TagSelector { get; }
 
-	public Tag Tag
+	/// <summary>Gets or sets the selected tag.</summary>
+	public Tag? Tag
 	{
 		get => _tag;
 		set => SetAndNotify(ref _tag, value);
 	}
 
-	/// <summary>
-	/// Gets a view model for creating a new product.
-	/// </summary>
+	/// <summary>Gets a view model for creating a new product.</summary>
 	public ProductCreationViewModel? ProductCreation
 	{
 		get => _productCreation;
-		private set => SetAndNotifyWithGuard(ref _productCreation, value, nameof(ProductCreation), nameof(CanCreateProduct));
+		private set =>
+			SetAndNotifyWithGuard(ref _productCreation, value, nameof(ProductCreation), nameof(CanCreateProduct));
 	}
 
-	/// <summary>
-	/// Gets a value indicating whether the transaction item can be created.
-	/// </summary>
+	/// <summary>Gets a value indicating whether the transaction item can be created.</summary>
 	public bool CanCreate =>
 		SourceAccount is not null &&
 		SourceAmount.HasValue &&
@@ -299,16 +274,13 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		Product is not null &&
 		_amount.HasValue;
 
-	/// <summary>
-	/// Gets a value indicating whether Update/Save button should be visible.
-	/// </summary>
+	/// <summary>Gets a value indicating whether Update/Save button should be visible.</summary>
 	public bool CanUpdate => _existingItem is not null;
 
+	/// <summary>Gets a value indicating whether a new product can be created.</summary>
 	public bool CanCreateProduct => ProductCreation is null;
 
-	/// <summary>
-	/// Asynchronously creates a new instance of the <see cref="TransactionItemCreationViewModel"/> class.
-	/// </summary>
+	/// <summary>Asynchronously creates a new instance of the <see cref="TransactionItemCreationViewModel"/> class.</summary>
 	/// <param name="gnomeshadeClient">Gnomeshade API client.</param>
 	/// <param name="itemId">The id of the transaction item to edit.</param>
 	/// <returns>A new instance of the <see cref="TransactionItemCreationViewModel"/> class.</returns>
@@ -316,30 +288,31 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		IGnomeshadeClient gnomeshadeClient,
 		Guid? itemId = null)
 	{
-		var accounts = await gnomeshadeClient.GetActiveAccountsAsync();
-		var currencies = await gnomeshadeClient.GetCurrenciesAsync();
-		var products = await gnomeshadeClient.GetProductsAsync();
-		var tags = await gnomeshadeClient.GetTagsAsync();
+		var accounts = await gnomeshadeClient.GetActiveAccountsAsync().ConfigureAwait(false);
+		var currencies = await gnomeshadeClient.GetCurrenciesAsync().ConfigureAwait(false);
+		var products = await gnomeshadeClient.GetProductsAsync().ConfigureAwait(false);
+		var tags = await gnomeshadeClient.GetTagsAsync().ConfigureAwait(false);
 
 		if (itemId is null)
 		{
 			return new(gnomeshadeClient, accounts, currencies, products, tags);
 		}
 
-		var existingItem = await gnomeshadeClient.GetTransactionItemAsync(itemId.Value);
-		return new(gnomeshadeClient, accounts, currencies, products, tags, existingItem);
+		var existingItem = await gnomeshadeClient.GetTransactionItemAsync(itemId.Value).ConfigureAwait(false);
+		var existingTags = await gnomeshadeClient.GetTransactionItemTagsAsync(itemId.Value).ConfigureAwait(false);
+		return new(gnomeshadeClient, accounts, currencies, products, tags, existingItem, existingTags);
 	}
 
-	/// <summary>
-	/// Updates an existing transaction item with the provided values.
-	/// </summary>
+	/// <summary>Updates an existing transaction item with the provided values.</summary>
 	/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
 	/// <exception cref="InvalidOperationException">One of the required values is not set.</exception>
 	public async Task SaveAsync()
 	{
 		if (_existingItem is null ||
-			SourceAccount is null || SourceCurrency is null ||
-			TargetAccount is null || TargetCurrency is null ||
+			SourceAccount is null ||
+			SourceCurrency is null ||
+			TargetAccount is null ||
+			TargetCurrency is null ||
 			Product is null)
 		{
 			throw new InvalidOperationException();
@@ -348,11 +321,9 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		var creationModel = new TransactionItemCreationModel
 		{
 			SourceAmount = SourceAmount,
-			SourceAccountId = SourceAccount.Currencies
-				.Single(inCurrency => inCurrency.Currency.Id == SourceCurrency.Id).Id,
+			SourceAccountId = SourceAccount.Currencies.Single(inCurrency => inCurrency.Currency.Id == SourceCurrency.Id).Id,
 			TargetAmount = TargetAmount,
-			TargetAccountId = TargetAccount.Currencies
-				.Single(inCurrency => inCurrency.Currency.Id == TargetCurrency.Id).Id,
+			TargetAccountId = TargetAccount.Currencies.Single(inCurrency => inCurrency.Currency.Id == TargetCurrency.Id).Id,
 			ProductId = Product.Id,
 			Amount = Amount,
 			BankReference = BankReference,
@@ -362,13 +333,16 @@ public class TransactionItemCreationViewModel : ViewModelBase
 			Description = null,
 		};
 
-		await _transactionClient.PutTransactionItemAsync(_existingItem.Id, _existingItem.TransactionId, creationModel);
+		await _transactionClient.PutTransactionItemAsync(_existingItem.Id, _existingItem.TransactionId, creationModel).ConfigureAwait(false);
+		foreach (var tag in TagRows)
+		{
+			await _transactionClient.TagTransactionItemAsync(_existingItem.Id, tag.Id).ConfigureAwait(false);
+		}
+
 		TransactionItemCreated?.Invoke(this, new(_existingItem.Id));
 	}
 
-	/// <summary>
-	/// Initializes <see cref="ProductCreation"/> so that a new product can be created.
-	/// </summary>
+	/// <summary>Initializes <see cref="ProductCreation"/> so that a new product can be created.</summary>
 	/// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
 	public async Task CreateProductAsync()
 	{
@@ -382,5 +356,16 @@ public class TransactionItemCreationViewModel : ViewModelBase
 		Products = Task.Run(() => _transactionClient.GetProductsAsync()).Result;
 		ProductCreation!.ProductCreated -= OnProductCreated;
 		ProductCreation = null;
+	}
+
+	private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
+	{
+		if (e.PropertyName is not nameof(Tag) || Tag is null)
+		{
+			return;
+		}
+
+		TagRows = new(TagRows.Append(new(Tag.Id, Tag.Name, Tag.Description)));
+		Tag = null;
 	}
 }
