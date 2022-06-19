86f669276be378e7cc1e16350d97d51b1eeee8a0
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-06-19 10:36:56 +0300

    todo - is this needed?

---
+++ source/Gnomeshade.Data/Migrations/00000016_name_uniqueness.sql
@@ -0,0 +1,12 @@
+﻿ALTER TABLE public.accounts
+	DROP CONSTRAINT accounts_normalized_name_counterparty_id;
+
+ALTER TABLE public.accounts
+	ADD CONSTRAINT accounts_normalized_name_counterparty_id
+		UNIQUE (counterparty_id, normalized_name) NOT DEFERRABLE;
+
+ALTER TABLE public.accounts
+	DROP CONSTRAINT IF EXISTS counterparties_normalized_name;
+
+ALTER TABLE public.counterparties
+	DROP CONSTRAINT IF EXISTS counterparties_normalized_name;
