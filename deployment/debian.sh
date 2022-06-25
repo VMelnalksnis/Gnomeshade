94452b87cfdde6ca3d2829afbecc7b2390fd5264
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-06-25 09:58:33 +0300

    feat(build): Add systemd unit, maintainer scripts to debian package

--- deployment/debian.sh
+++ deployment/debian.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install lintian || exit
-
 archive_path="$1"
 version=$(cat version)
 full_version="$version.$2"
-control_path="gnomeshade/DEBIAN/control"
-copyright_path="gnomeshade/usr/share/doc/gnomeshade/copyright"
 changelog_path="gnomeshade/usr/share/doc/gnomeshade/changelog.gz"
 maintainer_email="valters.melnalksnis@gnomeshade.org"
 maintainer="Valters Melnalksnis <$maintainer_email>"
@@ -17,30 +13,23 @@ unzip "$archive_path" -d gnomeshade/opt/gnomeshade || exit
 chmod +x gnomeshade/opt/gnomeshade/Gnomeshade.Interfaces.WebApi || exit
 
 mkdir -p gnomeshade/DEBIAN || exit
-echo "Package: gnomeshade" >>$control_path || exit
-echo "Version: $full_version" >>$control_path || exit
-echo "Section: misc" >>$control_path || exit
-echo "Priority: optional" >>$control_path || exit
-echo "Architecture: amd64" >>$control_path || exit
-echo "Maintainer: $maintainer" >>$control_path || exit
-echo "Description: The Gnomeshade server" >>$control_path || exit
-echo " Personal finance tracker server" >>$control_path || exit
-echo "Homepage: https://gnomeshade.org" >>$control_path || exit
+cp deployment/debian/postinst gnomeshade/DEBIAN/postinst || exit
+cp deployment/debian/prerm gnomeshade/DEBIAN/prerm || exit
+
+export FULL_VERSION=$full_version
+export MAINTAINER=$maintainer
+envsubst <deployment/debian/control >gnomeshade/DEBIAN/control || exit
 
 mkdir -p gnomeshade/etc/opt/gnomeshade || exit
 mv gnomeshade/opt/gnomeshade/appsettings.json gnomeshade/etc/opt/gnomeshade/appsettings.json
 echo "/etc/opt/gnomeshade/appsettings.json" >>gnomeshade/DEBIAN/conffiles || exit
 
 mkdir -p gnomeshade/usr/share/doc/gnomeshade || exit
-echo "Format: http://www.debian.org/doc/packaging-manuals/copyright-format/1.0/" >>$copyright_path || exit
-echo "Upstream-Name: gnomeshade" >>$copyright_path || exit
-echo "Upstream-Contact: $maintainer_email" >>$copyright_path || exit
-echo "Source: https://github.com/VMelnalksnis/Gnomeshade" >>$copyright_path || exit
-echo "" >>$copyright_path || exit
-echo "Files: *" >>$copyright_path || exit
-echo "Copyright: 2021 $maintainer" >>$copyright_path || exit
-echo "License: APGL-3.0-or-later" >>$copyright_path || exit
+export MAINTAINER_EMAIL=$maintainer_email || exit
+envsubst <deployment/debian/copyright >gnomeshade/usr/share/doc/gnomeshade/copyright || exit
 
+export TIME=$time
+envsubst <deployment/debian/changelog >changelog || exit
 echo "gnomeshade ($full_version) stable; urgency=low" >>changelog || exit
 echo "" >>changelog || exit
 echo "  * Placeholder" >>changelog || exit
@@ -49,6 +38,9 @@ echo " -- $maintainer  $time" >>changelog || exit
 
 gzip -n --best changelog || exit
 mv changelog.gz $changelog_path || exit
+
+mkdir -p gnomeshade/lib/systemd/system || exit
+cp deployment/debian/gnomeshade.service gnomeshade/lib/systemd/system/gnomeshade.service || exit
 
 dpkg-deb --root-owner-group --build gnomeshade || exit
 lintian --suppress-tags dir-or-file-in-opt,dir-or-file-in-etc-opt gnomeshade.deb || exit
