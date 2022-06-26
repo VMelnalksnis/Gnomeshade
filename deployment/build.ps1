5a9c9fd0cb1f4a21416426e3a63853e9f7be58a7
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-06-26 10:49:02 +0300

    feat(build): Build windows installer for desktop app

---
+++ deployment/build.ps1
@@ -0,0 +1,20 @@
+﻿param (
+    [Parameter(Mandatory = $true, Position = 0)]
+    [System.String]
+    $Project
+)
+
+$publish_dir = "source\$Project\bin\Release\"
+$archive_name = "${Project}.msi"
+
+$dotnetArgs = @()
+$dotnetArgs = $dotnetArgs + "build"
+$dotnetArgs = $dotnetArgs + ".\source\$Project\$Project.csproj"
+$dotnetArgs = $dotnetArgs + "--configuration" + "Release"
+$dotnetArgs = $dotnetArgs + "/warnAsError"
+$dotnetArgs = $dotnetArgs + "/nologo"
+
+& dotnet $dotnetArgs
+
+Write-Output "::set-output name=artifact-name::$Project"
+Write-Output "::set-output name=artifact::$publish_dir\$archive_name"
