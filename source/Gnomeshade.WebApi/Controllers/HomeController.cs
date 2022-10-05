bf6f30ada20a3c41133edfc6d7b3292e196a154b
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-10-05 17:47:36 +0300

    feat(api): Add default web UI

---
+++ source/Gnomeshade.WebApi/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2021 Valters Melnalksnis
+// Licensed under the GNU Affero General Public License v3.0 or later.
+// See LICENSE.txt file in the project root for full license information.
+
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gnomeshade.WebApi.Controllers;
+
+/// <summary>The home page and shared views.</summary>
+[AllowAnonymous]
+[Route("[controller]/[action]")]
+public sealed class HomeController : Controller
+{
+	/// <summary>Gets the home page.</summary>
+	/// <returns>The home page.</returns>
+	[HttpGet("/")]
+	public ViewResult Index() => View();
+}
