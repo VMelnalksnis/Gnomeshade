620a79da547bc4d305faf4937b2571792bf5a17f
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-10-23 11:45:10 +0300

    build: Fix package file name after pack

--- deployment/pack-nightly.sh
+++ deployment/pack-nightly.sh
@@ -3,8 +3,8 @@ version=$(cat version)
 publish_dir="./source/$1/bin/Release"
 full_version="$version.$2"
 package_version="$version-nightly$2"
-package_name="$1.$version.nupkg"
-symbols_name="$1.$version.snupkg"
+package_name="$1.$package_version.nupkg"
+symbols_name="$1.$package_version.snupkg"
 
 dotnet pack \
 	./source/"$1"/"$1".csproj \
