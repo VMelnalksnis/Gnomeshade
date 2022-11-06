902623e7c87c9ecb2844f18906c01d8a7ce5c3f5
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2022-11-06 18:28:15 +0200

    ci: Publish Gnomeshade.WebApi.Client to NuGet.org on release (#305)

--- deployment/pack-nightly.sh
+++ deployment/pack-nightly.sh
@@ -2,7 +2,11 @@
 version=$(cat version)
 publish_dir="./source/$1/bin/Release"
 full_version="$version.$2"
-package_version="$version-nightly$2"
+if [[ $3 == refs/tags/* ]]; then
+	package_version="$version"
+else
+	package_version="$version-nightly.$2"
+fi
 package_name="$1.$package_version.nupkg"
 symbols_name="$1.$package_version.snupkg"
 
@@ -18,8 +22,8 @@ dotnet pack \
 	/nologo ||
 	exit
 
-echo "artifact-name=$package_name" >> "$GITHUB_OUTPUT"
-echo "artifact=$publish_dir/$package_name" >> "$GITHUB_OUTPUT"
+echo "artifact-name=$package_name" >>"$GITHUB_OUTPUT"
+echo "artifact=$publish_dir/$package_name" >>"$GITHUB_OUTPUT"
 
-echo "symbols-name=$symbols_name" >> "$GITHUB_OUTPUT"
-echo "symbols=$publish_dir/$symbols_name" >> "$GITHUB_OUTPUT"
+echo "symbols-name=$symbols_name" >>"$GITHUB_OUTPUT"
+echo "symbols=$publish_dir/$symbols_name" >>"$GITHUB_OUTPUT"
