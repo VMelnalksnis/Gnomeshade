9c256ecc2a3c600dfa814ef6586642e6de0b8012
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2023-03-14 21:53:26 +0200

    ci: Fix vulnerable package check

--- deployment/nuget_vulnerabilities.sh
+++ deployment/nuget_vulnerabilities.sh
@@ -3,9 +3,13 @@ set -e
 
 ./deployment/restore.sh
 
-packages=$(dotnet list package --include-transitive --vulnerable)
+packages=$(dotnet list package --include-transitive --vulnerable --format json || true)
+echo "$packages"
 
-if [[ "$packages" == *"has the following vulnerable packages"* ]]; then
-	echo "$packages"
+if [[ "$packages" == *"vulnerabilities"* ]]; then
+	echo "Vulnerable packages"
+	exit 1
+elif [[ "$packages" != *"projects"* ]]; then
+	echo "Unexpected output"
 	exit 1
 fi
