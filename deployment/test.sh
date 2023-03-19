ea5d65a6d99478c227a772d5b5508d3d5a1642c8
Valters Melnalksnis <valters.melnalksnis@outlook.com>
2023-03-19 10:54:40 +0200

    build: Use coverlet.collector instead of coverlet.msbuild

--- deployment/test.sh
+++ deployment/test.sh
@@ -2,4 +2,4 @@
 set -e
 
 ./deployment/build.sh
-dotnet test -p:CollectCoverage=true -p:BuildInParallel=true -m:8 --configuration Release --no-build
+dotnet test -p:CollectCoverage=true -p:BuildInParallel=true -m:8 --configuration Release --collect:"XPlat Code Coverage" --no-build
